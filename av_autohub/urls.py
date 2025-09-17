7dfb4753e0be3a47f0b5ad3e9109ab413c12052a
Kc-Vic <6a9a218x@students.codeinstitute.net>
2025-09-17 22:25:49 +0100

    create home app and added templates

--- av_autohub/urls.py
+++ av_autohub/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),  # include allauth urls
+    path('', include('home.urls')),  # include home app urls
 ]
