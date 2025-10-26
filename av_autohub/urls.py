6cbbdd17a4fcf11860d189a7b96389a99e254ea8
Kc-Vic <6a9a218x@students.codeinstitute.net>
2025-10-26 19:42:10 +0000

    styled home page and added custom base.css file

--- av_autohub/urls.py
+++ av_autohub/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),  # include allauth urls
     path('', include('home.urls')),  # include home app urls
-]
+    path('home/', include('home.urls')),  # include home app urls
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
