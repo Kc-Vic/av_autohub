77570d2e935f3c617cb67b8e2bb2afa3f2cedca9
Kc-Vic <6a9a218x@students.codeinstitute.net>
2025-11-13 01:20:50 +0000

    Added description field to product model and created product detail page

--- products/models.py
+++ products/models.py
@@ -17,6 +17,7 @@ class Product(models.Model):
     condition = models.CharField(max_length=50, blank=True, null=True, choices=[('New', 'New'), ('Used', 'Used')], default='Used')
     credit_available = models.BooleanField(default=False)
     image = models.ImageField(blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
 
     def __str__(self):
         return self.title
