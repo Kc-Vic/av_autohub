5531876c96be0ca937e530930cc83ba741a36ed6
Kc-Vic <6a9a218x@students.codeinstitute.net>
2025-11-24 02:01:36 +0000

    setup up profiles template and views

--- profiles/views.py
+++ profiles/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import UserProfile
 
 # Create your views here.
 def profile(request):
-    context = {}
+    profiles = get_object_or_404(UserProfile, user=request.user)
+    context = {
+        'profiles': profiles,
+    }
     return render(request, 'profiles/profiles.html', context)
