4d26ba5bcae67b1686bff24a0a0eee75f6845ec1
Kc-Vic <6a9a218x@students.codeinstitute.net>
2025-11-26 06:18:30 +0000

    setting up heroku link

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn av_autohub.wsgi:application
--- av_autohub/settings.py
+++ av_autohub/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 from pathlib import Path
 from dotenv import load_dotenv
+import os
+import dj_database_url
+if os.path.isfile('.env.py'):
+    import env
 
 # Load environment variables from .env file
 load_dotenv()
@@ -29,7 +33,10 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'avautohub-3ceed5c741e2.herokuapp.com',
+    'localhost',
+]
 
 
 # Application definition
@@ -126,13 +133,17 @@ WSGI_APPLICATION = 'av_autohub.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
-
+else: 
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
