73f1ad44c9e2b8b6d18bdf3f6c76f7283d880ce5
Kc-Vic <6a9a218x@students.codeinstitute.net>
2025-11-26 12:23:28 +0000

    still troubleshooting static files

--- av_autohub/settings.py
+++ av_autohub/settings.py
@@ -164,14 +164,14 @@ if 'USE_AWS' in os.environ:
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
     
-    # Static Files
-    STATICFILES_STORAGE = 'custom_storages.StaticStorage' # Assumes you have defined custom_storages.py
+    # Static and media files
+    STATICFILES_STORAGE = 'custom_storages.StaticStorage'
     STATICFILES_LOCATION = 'static'
-    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
-
-    # Media Files
-    DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage' # Assumes custom_storages.py
+    DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
     MEDIAFILES_LOCATION = 'media'
+
+    # Override static and media URLs in production
+    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
 
 else:
