89442afd0e8558ce83b09932bac1a5f1a488a6dc
Kc-Vic <6a9a218x@students.codeinstitute.net>
2025-11-27 16:19:55 +0000

    updated aws expiry parameter

--- av_autohub/settings.py
+++ av_autohub/settings.py
@@ -155,6 +155,13 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'USE_AWS' in os.environ:
     # --- AWS CONFIGURATION ---
+    if 'USE_AWS' in os.environ:
+    # Cache control
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
+    }
+    
     AWS_STORAGE_BUCKET_NAME = 'avautohub'
     AWS_S3_REGION_NAME = os.environ.get('AWS_S3_REGION_NAME') or 'us-east-1' # Default to us-east-1 if not set
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
