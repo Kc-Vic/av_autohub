1c603978d43ebaff08403e9b7f94da422536262b
Kc-Vic <6a9a218x@students.codeinstitute.net>
2025-11-29 16:38:56 +0000

    Completed confirmation email settings after upgrading django to latest version. Set request in settings.py to pull info from environ

--- av_autohub/settings.py
+++ av_autohub/settings.py
@@ -186,13 +186,18 @@ else:
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_USE_TLS = True
-EMAIL_PORT = 587
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'akomavictor@gmail.com'
-EMAIL_HOST_PASSWORD = 'lnof hdyk vaoq ubok'
-DEFAULT_FROM_EMAIL = 'akomavictor@gmail.com'
+if 'DEVELOPMENT' in os.environ:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+    DEFAULT_FROM_EMAIL = 'avautohub@example.com'
+
+else:
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_USE_TLS = True
+    EMAIL_PORT = 587
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
+    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
 
 # PAYSTACK SETTINGS
 PAYSTACK_PUBLIC_KEY = os.environ.get('PAYSTACK_PUBLIC_KEY')
